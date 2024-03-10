a4838409ea1e7def338484d184dbf0bea731d13a
Gayathri-Godini3 <144263483+Gayathri-Godini3@users.noreply.github.com>
2024-03-10 10:28:04 +0530

    Updated  Alumini.js

--- step1/frontend/src/components/alumini/Alumini.js
+++ step1/frontend/src/components/alumini/Alumini.js
@@ -35,17 +35,17 @@ export default function Alumini(){
     }
     return (
     <>
-    alumin page
-    <h1>page</h1>
-        to ease the serach do use these suggested fields
+    
+    <center><h1>Almuni page</h1></center><hr />
+        <h3>To ease the search  use these suggested fields:</h3>
     <div className='container'>
-            suggestions:
-            <button name='year' onClick={suggestionHandler}>year of admisson</button>
-            <button name='branch' onClick={suggestionHandler}>branch</button>
-            <button name='compnay_name' onClick={suggestionHandler}>present company name</button>
+            <h4>Suggestions:</h4>
+            <button name='year' onClick={suggestionHandler} style={{marginRight:"20px",borderRadius:"10px"}}>Year of admisson</button>
+            <button name='branch' onClick={suggestionHandler} style={{marginRight:"20px",borderRadius:"10px"}}>Branch</button>
+            <button name='compnay_name' onClick={suggestionHandler} style={{marginRight:"20px",borderRadius:"10px"}}>Present Company name</button>
             
-        </div>
-        <form onSubmit={submitHandler}>
+        </div><hr />
+        {/* <form onSubmit={submitHandler}>
 
        
         id no:<input type="text" name="id_no" id="id_no" value={id_no} onChange={e=>setId_no(e.target.value)}/><br/>
@@ -58,5 +58,40 @@ export default function Alumini(){
         <p>
             the admin will look into the provided data and once admin validates them they will be displayed(for authenticity this featerue is added)
         </p>
+        */}
+        <form onSubmit={submitHandler}>
+            <center>
+            <table>
+                <tr>
+                    <th>Id No.</th>
+                    <td>
+                        <input type="text" name="id_no" id="id_no" value={id_no} onChange={e=>setId_no(e.target.value)} /> <br />
+
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Key:</th>
+                    <td>
+                        <input type="text" name="the_key" id="keyy" />
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Value:</th>
+                    <td>
+                        <input type="text" name="the_value" id="valuee" />
+                    </td>
+                    <td><input type="button" onClick={addAnotherHandler} value={"+"}/></td>
+                </tr>
+            </table><br />
+            <input type="submit" value={"Submit"}/>
+            </center>
+        </form>
+        <br />
+        <p>
+            The admin will look into the provided data and once admin validates them they will be displayed(for authenticity this featerue is added)
+        </p>
+
     </>)
 }
